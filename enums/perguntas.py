9bf7c42ae94860a7a7424f3833cd6e6703d39d1d
Caos <123587124+ryan-henrique-nantes@users.noreply.github.com>
2023-09-21 11:20:18 -0400

    SLA, TO PUTO
    AJuste da Tela.py

--- enums/perguntas.py
+++ enums/perguntas.py
@@ -1,10 +1,16 @@
 from enum import Enum
 
 class NumEscolhas(Enum):
-    VESTUARIO= 1
-    ATIVIDADES_EXTERNAS = 2
-    PREVISAO_CHUVA = 3
-    AVALIACAO_DE_CONFORTO = 4
-    EFICIENCIA_ENERGETICA = 5
-    SEGURANCA_VIARIA = 6
-    ATIVIDADES_AGRICOLAS = 7
+    VESTUARIO= "Que tipo de roupas devo usar hoje?"
+    ATIVIDADES_EXTERNAS = "O Clima esta bom para eu realizar alguma atividade ao ar livre?"
+    PREVISAO_CHUVA = "Devo levar um guarda-chuva?"
+    AVALIACAO_DE_CONFORTO = "A refrigeração interna, deve ser como?"
+    EFICIENCIA_ENERGETICA = "Como esta o clima para eu sair?"
+    SEGURANCA_VIARIA = "Como está o clima para realizar atividades agricolas?"
+    ATIVIDADES_AGRICOLAS = "O que posso usar para tornar o clima mais agradavel?"
+    
+    def values():
+        return [NumEscolhas.VESTUARIO.value, NumEscolhas.ATIVIDADES_EXTERNAS.value, 
+                NumEscolhas.PREVISAO_CHUVA.value, NumEscolhas.AVALIACAO_DE_CONFORTO.value, 
+                NumEscolhas.EFICIENCIA_ENERGETICA.value, NumEscolhas.SEGURANCA_VIARIA.value, 
+                NumEscolhas.ATIVIDADES_AGRICOLAS.value]
--- view/tela.py
+++ view/tela.py
@@ -1,26 +1,88 @@
 import sys
 import tkinter as tk
-sys.path.append('utils\Components')
-from formulario import TForm
-from labeled_edit import TLabeledEntry
-from botao import TButton
-from combobox import TComboBox
-from enums import Align_Text
+sys.path.append('utils')
+sys.path.append('enums')
+from tkinter import ttk
+from Components.formulario import TForm
+from Components.labeled_edit import TLabeledEntry
+from Components.botao import TButton
+from Components.combobox import TComboBox
+from Components.menus import TMenu
+from Frames_Enums.enums import Align_Text
+from perguntas import NumEscolhas
 
 class TelaConfig(TForm):
     def __init__(self):
-        super().__init__('Lógica Fuzzy', 350, 350, icone='utils\chimps.png')
+        super().__init__('Lógica Fuzzy', 550, 550, icone='icons/logo.png')
 
     def on_create(self):
-        self.lbeCidade = TLabeledEntry(self, callback_prefix='lbeCidade', label_alignment=Align_Text.LEFT, label_text='Informe a Localização')
-        self.btnBuscar = TButton(self, callback_prefix='btnBuscar', text='Perguntar')
-        self.cbxPergunta = TComboBox(self, callback_prefix='cbxPergunta')
+        self.mmuEntradas = TMenu(self)
+        self.mmuSaidas = TMenu(self)
+        self.mmuResultado = TMenu(self)
+        self.config(menu=self.mmuEntradas)
+        self.lblPergunta = tk.Label(self, text='Pergunta')
+        self.cbxPergunta = TComboBox(self, callback_prefix='cbxPergunta', width=55)
+        self.lbeCidade = TLabeledEntry(self, callback_prefix='lbeCidade', 
+                                       label_alignment=Align_Text.LEFT, 
+                                       label_text='Informe a Localização')
+        self.__populate()
 
-    def on_show(self, event):
-        self.lbeCidade.place(x=20, y=20)
-        self.cbxPergunta.place(x=200, y=40)
-        self.btnBuscar.place(x=135, y=100)
-        return super().on_show(event)
+    def on_show(self):
+        self.lblPergunta.place(x=20, y=20)
+        self.cbxPergunta.place(x=20, y=42)
+        self.lbeCidade.place(x=20, y=70)
+
+    def __populate(self):
+        self.cbxPergunta['values'] = NumEscolhas.values()
+        self.mmuEntradas.add_lista('Visualizar Entradas', 
+            ['Visualizar Nublado', 'Visualizar Temperatura', 'Visualizar Umidade', 
+             'Visualizar Intensidade Chuva', 'Visualizar Intensidade Neve',
+             'Visualizar Precipitação', 'Visualizar Temperatura Aparente',
+             'Visualizar Intensidade UV', 'Visualizar Visibilidade', 'Visualizar Condições Climáticas',
+             'Visualizar Rajada Vento', 'Visualizar Velocidade Vento'], 
+            [self.mmuVisualizarNublado_on_click, self.mmuVisualizarTemperatura_on_click,
+             self.mmuVisualizarUmidade_on_click, self.mmuVisualizarIntensidadeChuva_on_click,
+             self.mmuVisualizarIntensidadeNeve_on_click, self.mmuVisualizarPrecipitacao_on_click,
+             self.mmuVisualizarTemperaturaAparente_on_click, self.mmuVisualizarIntensidadeUV_on_click,
+             self.mmuVisualizarVisibilidade_on_click, self.mmuVisualizarCondicoesClimaticas_on_click,
+             self.mmuVisualizarRajadaVento_on_click, self.mmuVisualizarVelocidadeVento_on_click])
+        return self
+
+    def mmuVisualizarNublado_on_click(self):
+        print('a')
+        pass
     
+    def mmuVisualizarTemperatura_on_click(self):
+        pass
+    
+    def mmuVisualizarUmidade_on_click(self):
+        pass
+    
+    def mmuVisualizarIntensidadeChuva_on_click(self):
+        pass
+    
+    def mmuVisualizarIntensidadeNeve_on_click(self):
+        pass
+    
+    def mmuVisualizarPrecipitacao_on_click(self):
+        pass
+    
+    def mmuVisualizarTemperaturaAparente_on_click(self):
+        pass
+    
+    def mmuVisualizarIntensidadeUV_on_click(self):
+        pass
+    
+    def mmuVisualizarVisibilidade_on_click(self):
+        pass
+    
+    def mmuVisualizarCondicoesClimaticas_on_click(self):
+        pass
+    
+    def mmuVisualizarRajadaVento_on_click(self):
+        pass
+    
+    def mmuVisualizarVelocidadeVento_on_click(self):
+        pass
 
 TelaConfig().mainloop()
